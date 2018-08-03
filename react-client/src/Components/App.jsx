a62aba1a1b3236ec33bf25492fcff94fde2ec2ee
dishabendre <dishabendre@gmail.com>
2018-08-03 00:04:27 -0700

    updated homepage, navbar

---
+++ react-client/src/Components/App.jsx
@@ -0,0 +1,23 @@
+// component sotres routes for the various React routes used to navigate the website
+// using React-Router:
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import HomePage from './HomePage';
+import NavBar from './HeaderComponent/NavBar';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar/>
+                    <Route name="name" exact path="/" component={HomePage}  />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,1 +1,8 @@
 // this file serves as the entry point of the application where we point to all our .jsx files
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+
+// <App /> is the first component to be rendered to virtual DOM
+ReactDOM.render(<App />, document.getElementById('app'));
